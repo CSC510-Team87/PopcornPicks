eef70b270d2ff790471de719666924178a8827b6
Yi Zhang <zy2803414747@gmail.com>
2024-11-26 22:38:29 -0500

    Fixed wiring

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,20 +1,7 @@
 "use client";
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import React from "react";
+import { redirect } from "next/navigation";
 
 export default function Page() {
-    const router = useRouter();
-
-    useEffect(() => {
-      router.push('/login');
-      // This code runs only on the client side
-      const token = localStorage.getItem('token');
-      if (!token) {
-          // Redirect to login if no token is found
-          router.push('/login');
-      }
-    }, [router]);
-
-    return <div className="w-full"></div>;
+  return <div className="w-full">{localStorage.getItem("token") && redirect("/login")}</div>;
 }
-
